f0131b034d063a12b1aa40076a4c22cc8ec367ce
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-24 03:25:56 +0300

    I did exactly as he does, but there's an error

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,15 @@
+//get the seconds hand
+const secondHand = document.querySelector('.second-hand');
+
+const setDate= function () {
+    //get today's date
+    const now= new Date();
+    //get the seconds
+    const seconds= now.getSeconds();
+    console.log(seconds);
+    //we need degrees
+    const secondsDegrees= ((seconds/60) *360) + 90;
+    secondHand.style.transform=`rotate (${secondsDegrees}deg)`
+}
+
+setInterval(setDate, 1000);
