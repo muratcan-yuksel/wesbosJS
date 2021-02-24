a05da5f77fa8bcf6d9f5ce306c851f9340c948fb
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-24 03:56:38 +0300

    the bug : I forgot the dot in query selector

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,7 +1,7 @@
 //get the hands
 const secondHand = document.querySelector('.second-hand');
 const minHand= document.querySelector(".min-hand");
-const hourHand = document.querySelector("hour-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 
 const setDate= function () {
@@ -17,9 +17,17 @@ const setDate= function () {
 
     //get the minds
     const minutes= now.getMinutes();
-    const minutesDeg= ((minutes/60)*360 + 90);
+    const minutesDeg= ((minutes/60)*360) + 90;
     minHand.style.transform= `rotate(${minutesDeg}deg)`;
 
+   
+    //get the hour
+    const hours= now.getHours();
+    const hoursDeg= ((hours/12)*360) + 90;
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+
+   
+   
 
 }
 
