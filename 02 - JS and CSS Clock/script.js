c1728624952025bbf9cbea51fea2a872cd8e6b9e
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-24 03:43:26 +0300

    added the minutes

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,15 +1,25 @@
-//get the seconds hand
+//get the hands
 const secondHand = document.querySelector('.second-hand');
+const minHand= document.querySelector(".min-hand");
+const hourHand = document.querySelector("hour-hand");
+
 
 const setDate= function () {
     //get today's date
     const now= new Date();
+
     //get the seconds
     const seconds= now.getSeconds();
     console.log(seconds);
     //we need degrees
     const secondsDegrees= ((seconds/60) *360) + 90;
     secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
+
+    //get the minds
+    const minutes= now.getMinutes();
+    const minutesDeg= ((minutes/60)*360 + 90);
+    minHand.style.transform= `rotate(${minutesDeg}deg)`;
+
 
 }
 
